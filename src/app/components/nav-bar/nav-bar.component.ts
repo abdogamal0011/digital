df697af1374733a4e4243acbca3cd2f62a2cb865
abdeelrhman gamal <abdogamal000@gmail.com>
2024-11-19 19:33:17 +0200

    first

---
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css'
+})
+export class NavBarComponent {
+  image:string="../../../assets/logo.webp"
+  isNavVisible = false;
+
+  toggleNav() {
+    this.isNavVisible = !this.isNavVisible;
+  }
+}
---
+++ src/app/components/review/review.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
+
+@Component({
+  selector: 'app-review',
+  standalone: true,
+  imports: [CarouselModule],
+  templateUrl: './review.component.html',
+  styleUrl: './review.component.css',
+  animations:[trigger('backInRight', [
+    state('void', style({ opacity: 0, transform: 'translateX(100%)' })),
+    transition(':enter', [
+      animate('1s ease-in', style({ opacity: 1, transform: 'translateX(0)' }))
+    ]),
+    transition(':leave', [
+      animate('1s ease-out', style({ opacity: 0, transform: 'translateX(100%)' }))
+    ])
+  ])
+]
+})
+export class ReviewComponent {
+  image2:string='../../../assets/client-1.png';
+  image3:string='../../../assets/client-2.png';
+  image4:string='../../../assets/client-3.png';
+  image5:string='../../../assets/client-4.png';
+  image6:string='../../../assets/client-5.png';
+  image7:string='../../../assets/client-6.png';
+  image8:string='../../../assets/client-7.png';
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 400,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  }
+}
