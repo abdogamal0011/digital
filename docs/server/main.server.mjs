abcee7c4dbd66be2c8c9aeca1383ffcc54b7ab71
abdeelrhman gamal <abdogamal000@gmail.com>
2024-11-22 16:53:06 +0200

    final

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-T33AWRHW.mjs";import"./chunk-USOI5BWL.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-G7MP3547.mjs";import"./chunk-NP2WGLVM.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
-  image:string="./assets/logo.webp"
+  image:string="../../../assets/image/Logo_V2.gif"
   isNavVisible = false;
 
   toggleNav() {
